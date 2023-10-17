f69b89fd3af07b4961edee53689a3b5ef488de62
smc <alsch4013@gmail.com>
2023-10-17 09:20:19 +0900

    Tue Oct 17 09:20:19     2023 By Mincho

--- workspace.edu/SpringMVC10/src/main/java/kr/spring/MySQL.sql
+++ workspace.edu/SpringMVC10/src/main/java/kr/spring/MySQL.sql
@@ -1,1 +1,16 @@
 SELECT * FROM BOARD;
+
+INSERT INTO BOARD (TITLE, CONTENT, WRITER)
+VALUES ('오늘 저녁에 하늘마당에서 보실 분', '내가 치킨 쏜다', '명훈쌤');
+
+INSERT INTO BOARD (TITLE, CONTENT, WRITER)
+VALUES ('안녕하세요 손석구입니다!', '너 낙지된거야', '손썩구');
+
+INSERT INTO BOARD (TITLE, CONTENT, WRITER)
+VALUES ('이제 귀찮아요', '쓰는거', '힝');
+
+INSERT INTO BOARD (TITLE, CONTENT, WRITER)
+VALUES ('이번주 금요일 시간 어때요', '곱창맛있겠다', '준용쌤');
+
+INSERT INTO BOARD (TITLE, CONTENT, WRITER)
+VALUES ('커피 먹고싶다', '오트 바닐라', '매머드');
