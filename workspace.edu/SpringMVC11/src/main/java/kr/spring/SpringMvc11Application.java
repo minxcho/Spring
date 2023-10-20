2c157d8f9fff25a7e62e43056f9155530fe6a46b
smc <alsch4013@gmail.com>
2023-10-20 17:36:11 +0900

    Fri Oct 20 17:36:10     2023 By Mincho

--- workspace.edu/SpringMVC11/src/main/java/kr/spring/SpringMvc11Application.java
+++ workspace.edu/SpringMVC11/src/main/java/kr/spring/SpringMvc11Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMvc11Application {
 
+	
 	public static void main(String[] args) {
 		// Spring Boot를 구동 및 실행하는 클래스
 		SpringApplication.run(SpringMvc11Application.class, args);
--- workspace.edu/SpringMVC11/src/main/java/kr/spring/config/UserDetailsServiceImpl.java
+++ workspace.edu/SpringMVC11/src/main/java/kr/spring/config/UserDetailsServiceImpl.java
@@ -31,8 +31,6 @@ public class UserDetailsServiceImpl implements UserDetailsService{
 
 	
 	
-	
-	
 }
 
 
